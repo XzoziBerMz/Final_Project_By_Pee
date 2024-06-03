88c4c39f22575645231f64ceb04039f7c28b6721
XzoziBerMz <xzoziber.mz@gmail.com>
2024-06-03 22:04:24 +0700

    Test1

--- book.php
+++ book.php
@@ -16,7 +16,6 @@ require_once  "header.php";
 
   <!-- Link to Font Awesome for icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-
   <!-- css -->
   <link rel="stylesheet" href="Custom/body.css">
   <link rel="stylesheet" href="Custom/book.css">
