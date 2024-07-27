939044a21c856bf5c7bd2efd7ead7220a3d8b4b4
XzoziBerMz <xzoziber.mz@gmail.com>
2024-07-27 15:47:00 +0700

    Fix delete notify

--- update_notify.php
+++ update_notify.php
@@ -6,13 +6,11 @@ require_once 'connetdatabase/conn_db.php';
 $data = json_decode(file_get_contents('php://input'), true);
 $notify_id = $data['id'];
 
-// อัปเดตสถานะการแจ้งเตือนเฉพาะ notify_id ที่ระบุ
-$update_query = "UPDATE notify SET notify_status = :notify_status WHERE id = :notify_id";
-$update_stmt = $conn->prepare($update_query);
-$new_status = false; // ค่าสถานะใหม่
-$update_stmt->bindParam(':notify_status', $new_status, PDO::PARAM_BOOL);
-$update_stmt->bindParam(':notify_id', $notify_id, PDO::PARAM_INT);
-$update_stmt->execute();
+// ลบแถวที่มี notify_id ที่ระบุ
+$delete_query = "DELETE FROM notify WHERE id = :notify_id";
+$delete_stmt = $conn->prepare($delete_query);
+$delete_stmt->bindParam(':notify_id', $notify_id, PDO::PARAM_INT);
+$delete_stmt->execute();
 
 // ส่งผลลัพธ์กลับไปยัง JavaScript
 echo json_encode(['status' => 'success']);
